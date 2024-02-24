0e89c3f73963a5b45b9a7b5490fa55d1bb8f318d
Raquel <137739416+Luminous-Moon@users.noreply.github.com>
2024-02-24 16:48:05 +1100

    Create Test.groovy

---
+++ Test.groovy
@@ -0,0 +1,13 @@
+class Test {
+  //24/2/24
+   static void main(String[] args) {
+     print("Whats your name?")
+    def name = System.in.newReader().readLine()
+    
+    String output = "Hello, "
+      
+      println(output + name)
+      //Hello Yourname
+      
+   } 
+}
